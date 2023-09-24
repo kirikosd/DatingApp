4d1f9bd6fbf2278b8a08a616bbc2cc9361f250da
Kirykos Doumanis <kirikosdoumanis@gmail.com>
2023-09-24 13:42:35 +0300

    End of section 18

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -36,11 +36,6 @@ namespace API.Data
 
         }
 
-        public async Task<bool> SaveAllAsync()
-        {
-            return await _context.SaveChangesAsync() > 0;
-        }
-
         public void Update(AppUser user)
         {
             _context.Entry(user).State = EntityState.Modified;
@@ -76,6 +71,14 @@ namespace API.Data
                 .Where(x => x.UserName == username)
                 .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
                 .SingleOrDefaultAsync();
+        }
+
+        public async Task<string> GetUserGender(string username)
+        {
+            return await _context.Users
+                .Where(x => x.UserName == username)
+                .Select(x => x.Gender)
+                .FirstOrDefaultAsync();
         }
     }
 }
